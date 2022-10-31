af1316fdfc7a10f9f1c3506f80ace9d17403fa26
Tetiana Deineka <tata4ka666@i.ua>
2022-10-31 19:59:44 +0200

    Task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+const items = []
+
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add("gallery__item");
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add("gallery__link");
+    galleryLink.href = element.original;
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute("data-source", element.original);
+    galleryImage.alt = element.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+    items.push(galleryItem);
+})    
+
+gallery.append(...items);
